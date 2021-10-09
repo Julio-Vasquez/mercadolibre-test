9c38721f3ed19e3e25b448d0699b0696b7b8672c
Julio-Vasquez <jualvalitube@gmail.com>
2021-10-09 12:52:00 -0500

    merge acept change

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import { Provider } from 'react-redux'
 
 import Store from './store/Store'
 
-<<<<<<< HEAD
 import './sass/_all.scss'
-=======
-import 'antd/dist/antd.css'
-import App from './views/App'
->>>>>>> dfl
 
 ReactDOM.render(
   <Provider store={Store}>
