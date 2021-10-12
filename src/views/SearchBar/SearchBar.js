c095eaa9bf61fef4ba06217b14fcc310853a21e2
Julio <jualvalitube@gmail.com>
2021-10-12 01:33:03 -0500

    feat(SearchBar): structure and styles

--- src/views/SearchBar/SearchBar.js
+++ src/views/SearchBar/SearchBar.js
@@ -1,10 +1,48 @@
-import { search } from './SearchBar.module.scss'
+import { useState } from 'react'
+
+import { Col, Row, Input, AutoComplete } from 'antd'
+import { search, logo, bar } from './SearchBar.module.scss'
+
+import LogoML from '../../assets/svg/favicon.svg'
 
 export const SearchBar = () => {
+  const [options, setOptions] = useState([])
+  const onSelect = value => {
+    console.log('onSelect', value)
+  }
+
   return (
-    <div className={search}>
-      <span>{'=><='}</span>
-    </div>
+    <Col className={search}>
+      <Row gutter={[16, { xs: 8, sm: 16, md: 24, lg: 32 }]}>
+        <Col
+          className="gutter-row"
+          offset={4}
+          span={4}
+          style={{ display: 'flex', alignItems: 'center' }}
+        >
+          <div className={logo}>
+            <img src={LogoML} alt="Mercado Libre argentina" />
+          </div>
+        </Col>
+        <Col
+          className="gutter-row"
+          span={16}
+          style={{ display: 'flex', alignItems: 'center' }}
+        >
+          <div className={bar}>
+            <AutoComplete
+              style={{
+                width: 700,
+              }}
+              onSelect={onSelect}
+              options={options}
+            >
+              <Input.Search size="large" placeholder="Buscar" enterButton />
+            </AutoComplete>
+          </div>
+        </Col>
+      </Row>
+    </Col>
   )
 }
 
