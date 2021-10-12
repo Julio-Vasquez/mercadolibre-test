0ea3d332691a82099b3282880b89a80602b98f15
Julio <jualvalitube@gmail.com>
2021-10-12 01:42:14 -0500

    feat(SearchBar): autcomplete bar functional

--- src/views/SearchBar/SearchBar.js
+++ src/views/SearchBar/SearchBar.js
@@ -5,8 +5,47 @@ import { search, logo, bar } from './SearchBar.module.scss'
 
 import LogoML from '../../assets/svg/favicon.svg'
 
+function getRandomInt(max, min = 0) {
+  return Math.floor(Math.random() * (max - min + 1)) + min // eslint-disable-line no-mixed-operators
+}
+
+const searchResult = query =>
+  new Array(getRandomInt(5))
+    .join('.')
+    .split('.')
+    .map((_, idx) => {
+      const category = `${query}${idx}`
+      return {
+        value: category,
+        label: (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <span>
+              Found {query} on{' '}
+              <a
+                href={`https://s.taobao.com/search?q=${query}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {category}
+              </a>
+            </span>
+            <span>{getRandomInt(200, 100)} results</span>
+          </div>
+        ),
+      }
+    })
+
 export const SearchBar = () => {
   const [options, setOptions] = useState([])
+
+  const handleSearch = value => {
+    setOptions(value ? searchResult(value) : [])
+  }
   const onSelect = value => {
     console.log('onSelect', value)
   }
@@ -36,6 +75,7 @@ export const SearchBar = () => {
               }}
               onSelect={onSelect}
               options={options}
+              onSearch={handleSearch}
             >
               <Input.Search size="large" placeholder="Buscar" enterButton />
             </AutoComplete>
